e34ac999f8f4f6df5fe87d325fbc3a6f7633e426
Vincent Junior <Vincent.halimjr@gmail.com>
2024-07-09 15:47:45 +0700

    Initial Commit

---
+++ ProductDetail/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProductDetail
+//
+//  Created by Vincent Junior Halim on 09/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProductDetail/ProductDetailApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductDetailApp.swift
+//  ProductDetail
+//
+//  Created by Vincent Junior Halim on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ProductDetailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
