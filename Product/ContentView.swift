7230dca1b8e35fbfdea8cb1294eca73a1150ddd6
Vincent Junior <Vincent.halimjr@gmail.com>
2024-07-10 19:52:52 +0700

    initial commit

---
+++ Product/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  ProductDetail
+//
+//  Created by Vincent Junior Halim on 09/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(.iphone)
+                .resizable()
+                .scaledToFit()
+            .frame(width: 250,height: 350)
+            Text("Iphone 15")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(10)
+            Text("The iPhone 15 comes with 6.1-inch OLED displsay and Apple Bionic 16 processor. On the Back there is a Dual camera setup with 48MP main camera and 12MP Ultra-wide sensor.")
+                .padding(.horizontal,16)
+                .multilineTextAlignment(.center)
+            
+            Button(action:{},
+                   label: {
+                Text("Add To Cart")
+                    .padding(8)
+                    .foregroundStyle(.text)
+            })
+            .frame(width: 325)
+            .background(.button)
+            .clipShape(RoundedRectangle(cornerRadius: 14))
+            .padding(10)
+            
+            
+        }
+        .padding(10)
+        
+        
+        
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
